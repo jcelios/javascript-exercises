c20832e9f18a32131985511cad319a2b7795d20d
jcelios <194264137+jcelios@users.noreply.github.com>
2025-02-07 14:03:29 -0500

    Completed Exercises 01 to 07 for Foundations Course - Arrays and Loops.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  for (let x of args) {
+    if (arr.includes(x)) {
+      // arr.splice(arr.indexOf(x), 1);
+      arr = arr.filter(y => y !== x);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let c = 0;
+  if (a > 0 && Number.isInteger(a) && typeof b === "number") {
+    for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+      c += i;
+    }
+    return c;
+  } else {
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
